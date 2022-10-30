73a81361f18de4b546ea8a07b004517fdd9fc4e8
Dvir Shaul <dvir.shaul@gmail.com>
2022-10-30 13:50:07 +0200

    user controller update

--- src/main/java/Controllers/AuthenticationService.java
+++ src/main/java/Controllers/AuthenticationService.java
@@ -1,6 +1,26 @@
 package main.java.Controllers;
 
 class AuthenticationService {
+    
+    
+    
+    
 
+    public static void updatePassword(String password) {
+        
+    }
+
+    public static void updateName(String name) {
+
+    }
+
+    public static void updateEmail(String mail) {
+        
+    }
+
+
+    public static void deleteUser(User user) {
+
+    }
 }
 
--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -2,5 +2,26 @@ package main.java.Controllers;
 
 public class UserController {
 
+
+
+
+    public static void updateEmail(String mail){
+        AuthenticationService.updateEmail(mail);
+
+    }
+
+    public static void updateName(String name){
+        AuthenticationService.updateName(name);
+    }
+    public static void updatePassword(String password){
+        AuthenticationService.updatePassword(password);
+    }
+
+
+    public static void deleteUser(User user){
+        AuthenticationService.deleteUser(user);
+
+    }
+
 }
 
