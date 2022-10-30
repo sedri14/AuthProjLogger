38b26b3e81a4eba86a029f4c9e09e5f8dcce8d55
Dvir Shaul <dvir.shaul@gmail.com>
2022-10-30 16:02:32 +0200

    added user update workflow

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -1,25 +1,36 @@
 package Controllers;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class UserController {
 
-
-
-
-    public static void updateEmail(String mail){
-        AuthenticationService.updateEmail(mail);
-
+    public static void updateEmail(String mail, String token) throws IOException {
+//        if (mail == null)
+//            return;
+//        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+//                "[a-zA-Z0-9_+&*-]+)*@" +
+//                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+//                "A-Z]{2,7}$";
+//        Pattern pat = Pattern.compile(emailRegex);
+//        if(pat.matcher(mail).matches()) {
+        User user = AuthenticationService.validate(token);
+        UserService.updateEmail(user, mail);
     }
 
-    public static void updateName(String name){
-        AuthenticationService.updateName(name);
+    public static void updateName(String name, String token) throws IOException {
+        User user = AuthenticationService.validate(token);
+        UserService.updateName(user,name);
     }
-    public static void updatePassword(String password){
-        AuthenticationService.updatePassword(password);
+    public static void updatePassword(String password, String token) throws IOException {
+        User user = AuthenticationService.validate(token);
+        UserService.updatePassword(user,password);
     }
 
 
-    public static void deleteUser(User user){
-        AuthenticationService.deleteUser(user);
+    public static void deleteUser(String token){
+        User user = AuthenticationService.validate(token);
+        UserService.deleteUser(user);
 
     }
 
