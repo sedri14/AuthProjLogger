50daeb5d83d058685f02bae713c2d0292940eb8e
Dvir Shaul <dvir.shaul@gmail.com>
2022-10-31 15:16:11 +0200

    change controller to return boolean

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -13,33 +13,23 @@ public class UserController {
         this.userService = UserService.getInstance();
     }
 
-    public void updateEmail(String mail, String token) throws IOException {
-//        if (mail == null)
-//            return;
-//        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-//                "[a-zA-Z0-9_+&*-]+)*@" +
-//                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-//                "A-Z]{2,7}$";
-//        Pattern pat = Pattern.compile(emailRegex);
-//        if(pat.matcher(mail).matches()) {
+    public boolean updateEmail(String mail, String token) throws IOException {
         User user = authService.validate(token);
-        userService.updateEmail(user, mail);
+        return userService.updateEmail(user, mail);
     }
 
-    public void updateName(String name, String token) throws IOException {
+    public boolean updateName(String name, String token) throws IOException {
         User user = authService.validate(token);
-        userService.updateName(user,name);
+        return userService.updateName(user,name);
     }
-    public void updatePassword(String password, String token) throws IOException {
+    public boolean updatePassword(String password, String token) throws IOException {
         User user = authService.validate(token);
-        userService.updatePassword(user,password);
+        return userService.updatePassword(user,password);
     }
 
-
-    public void deleteUser(String token){
+    public boolean deleteUser(String token){
         User user = authService.validate(token);
-        userService.deleteUser(user);
-
+        return userService.deleteUser(user);
     }
 
 }
